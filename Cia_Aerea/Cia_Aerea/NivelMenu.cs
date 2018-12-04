07bb85262d43056430a802b66c30d4f64ee8384a
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-12-04 17:24:39 -0200

    Add files via upload

---
+++ Cia_Aerea/Cia_Aerea/NivelMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cia_Aerea
+{
+    public enum NivelMenu
+    {
+        Primeiro,
+        Segundo,
+        Terceiro
+    }
+}
